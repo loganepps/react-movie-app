1459465fd63f776bde2ad991d3b0c2900511ac18
loganepps <logepps@gmail.com>
2025-08-21 14:05:36 -0500

    connected appwrite database and added trending movies section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { getTrendingMovies, updateSearchCount } from "./appwrite";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import MovieCard from "./components/MovieCard";
-import Search from "./components/Search";
-import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard.jsx";
+import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
 import { useDebounce } from "react-use";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
@@ -21,13 +20,12 @@ const API_OPTIONS = {
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const [movieList, setMovieList] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
-  const [trendingMovies, setTrendingMovies] = useState([]);
 
-  // Debounce the search term to prevent making too many API requests
-  // by waiting for the user to stop typing for 500ms
+  // debounce the search term to prevent making too many API requests
+  // waits for user to stop typing for 500ms
   useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
   const fetchMovies = async (query = "") => {
@@ -36,7 +34,7 @@ const App = () => {
 
     try {
       const endpoint = query
-        ? `${API_BASE_URL}/search/movie?query=${encodeURI(query)}`
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
         : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
@@ -47,17 +45,13 @@ const App = () => {
 
       const data = await response.json();
 
-      if (data.success === false) {
+      if (data.Response === "False") {
         setErrorMessage(data.Error || "Failed to fetch movies");
-        setMovieList([]);
+        setMovies([]);
         return;
       }
 
-      setMovieList(data.results || []);
-
-      if (query && data.results.length > 0) {
-        await updateSearchCount(query, data.results[0]);
-      }
+      setMovies(data.results || []);
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage("Error fetching movies. Please try again later.");
@@ -66,23 +60,9 @@ const App = () => {
     }
   };
 
-  const loadTrendingMovies = async () => {
-    try {
-      const movies = await getTrendingMovies();
-
-      setTrendingMovies(movies);
-    } catch (error) {
-      console.error(`Error fetching trending movies: ${error}`);
-    }
-  };
-
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
-
-  useEffect(() => {
-    loadTrendingMovies();
-  }, []);
 
   return (
     <main>
@@ -91,32 +71,16 @@ const App = () => {
       <div className="wrapper">
         <header>
           <img src="./hero.png" alt="Hero Banner" />
-          <h1>
-            Find <span className="text-gradient-1">Movies</span>
-            You'll Enjoy
-            <h2>
-              <span className="text-gradient-2">Without the Hassle</span>
-            </h2>
+          <h1 className="mt-[30px]">
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
           </h1>
+
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        {trendingMovies.length > 0 && (
-          <section className="trending">
-            <h2>Trending Movies</h2>
-            <ul>
-              {trendingMovies.map((movie, index) => (
-                <li key={movie.$id}>
-                  <p>{index + 1}</p>
-                  <img src={movie.poster_url} alt={movie.title} />
-                </li>
-              ))}
-            </ul>
-          </section>
-        )}
-
         <section className="all-movies">
-          <h2>All Movies</h2>
+          <h2 className="mt-[40px]">All Movies</h2>
 
           {isLoading ? (
             <Spinner />
@@ -124,7 +88,7 @@ const App = () => {
             <p className="text-red-500">{errorMessage}</p>
           ) : (
             <ul>
-              {movieList.map((movie) => (
+              {movies.map((movie) => (
                 <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,5 @@ import { createRoot } from "react-dom/client";
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <App />
-  /* </StrictMode>, */
+  // </StrictMode>
 );
